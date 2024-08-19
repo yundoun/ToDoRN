a70b36d81cbafe2cad9be61697eb405a3ec129bf
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2024-08-19 15:30:18 +0900

    Feat : 기능 완성

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MainScreen from './screens/MainScreen';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 export default function App() {
+
   return (
-    <MainScreen />
+    <Provider store={store}>
+      <MainScreen />
+    </Provider>
   );
 }
 
--- components/InputForm.js
+++ components/InputForm.js
@@ -1,15 +1,31 @@
 import { KeyboardAvoidingView, Platform, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../redux/slices/todoSlice';
 
 const InputForm = () => {
+  const [currentValue, setCurrentValue] = React.useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = () => {
+    if (currentValue != '') {
+      dispatch(addTodo(currentValue));
+      setCurrentValue('');
+    }
+  }
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={styles.addFormContainer}>
       <TextInput
         style={styles.inputField}
-        placeholder={'할 일을 입력해주세요'} />
-      <Pressable style={styles.addButton}>
+        placeholder={'할 일을 입력해주세요'}
+        value={currentValue}
+        onChangeText={setCurrentValue}
+        onSubmitEditing={handleSubmit}
+      />
+      <Pressable style={styles.addButton} onPress={handleSubmit}>
         <Text style={styles.addButtonText}>+</Text>
       </Pressable>
     </KeyboardAvoidingView>
