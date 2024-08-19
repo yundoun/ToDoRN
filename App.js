3d0dbad5abb7ba63b18e55adfa8e6c959e9f9e79
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2024-08-19 16:21:39 +0900

    Feat : Login UI 구현

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native';
 import MainScreen from './screens/MainScreen';
+import LoginScreen from './screens/LoginScreen';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-import LoginScreen from './screens/LoginScreen';
 
 export default function App() {
 
@@ -14,8 +14,8 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen options={{ headerShown: false }} name="Main" component={MainScreen} />
           <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+          {/* <Stack.Screen options={{ headerShown: false }} name="Main" component={MainScreen} /> */}
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,140 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import ListIcon from '../assets/list.svg'
 
 const LoginScreen = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // useEffect(() => {
+  //   const unsubscribe = onAuthStateChanged(auth, user => {
+  //     if (user) {
+  //       navigation.replace("Main")
+  //     }
+  //   })
+  //   return unsubscribe;
+  // }, [])
+
+  const handleSignUp = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, email, password);
+      console.log('user', user);
+      Toast.show({
+        type: 'success',
+        text1: '회원가입 성공',
+        text2: `${email}으로 가입되었습니다.`
+      })
+    } catch (error) {
+      console.log(error.message);
+      Alert.alert(
+        "회원가입 도중에 문제가 발생했습니다.",
+        error.message,
+        [{ text: '닫기', onPress: () => console.log('닫기') }],
+        { cancelable: true }
+      )
+    }
+  }
+
+  const handleLogin = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(auth, email, password);
+      console.log(user);
+    } catch (error) {
+      Alert.alert(
+        "로그인 도중에 문제가 발생했습니다.",
+        error.message,
+        [{ text: '닫기', onPress: () => console.log('닫기') }],
+        { cancelable: true }
+      )
+    }
+  }
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View
+      style={styles.container}
+    >
+      <ListIcon />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="이메일"
+          value={email}
+          onChangeText={text => setEmail(text)}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="비밀번호"
+          value={password}
+          onChangeText={text => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleLogin}
+        >
+          <Text style={styles.buttonText}>로그인</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.button, styles.buttonOutline]}
+          onPress={handleSignUp}
+        >
+          <Text styles={styles.buttonOutlineText}>회원가입</Text>
+        </TouchableOpacity>
+      </View>
     </View>
+
   )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: '80%',
+    marginTop: 15
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5
+  },
+  buttonContainer: {
+    width: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30
+  },
+  button: {
+    backgroundColor: 'black',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center'
+  },
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '500',
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: 'black',
+    fontWeight: '500',
+    fontSize: 16
+  }
+})
