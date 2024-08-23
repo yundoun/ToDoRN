3a4535ccc92bf83d9a232c6b3703d3eba67388d9
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2024-08-23 09:48:34 +0900

    Feat : 회원가입 구현

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,40 +1,22 @@
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import ListIcon from '../assets/list.svg'
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
 
 const LoginScreen = () => {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-  // useEffect(() => {
-  //   const unsubscribe = onAuthStateChanged(auth, user => {
-  //     if (user) {
-  //       navigation.replace("Main")
-  //     }
-  //   })
-  //   return unsubscribe;
-  // }, [])
+  const auth = getAuth();
 
   const handleSignUp = async () => {
     try {
       const user = await createUserWithEmailAndPassword(auth, email, password);
       console.log('user', user);
-      Toast.show({
-        type: 'success',
-        text1: '회원가입 성공',
-        text2: `${email}으로 가입되었습니다.`
-      })
     } catch (error) {
       console.log(error.message);
-      Alert.alert(
-        "회원가입 도중에 문제가 발생했습니다.",
-        error.message,
-        [{ text: '닫기', onPress: () => console.log('닫기') }],
-        { cancelable: true }
-      )
     }
-  }
+  };
 
   const handleLogin = async () => {
     try {
